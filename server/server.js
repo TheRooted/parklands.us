110ca16dc3c2ad8e6e4bee73aada7b5e54527f35
bschult <brianjschultz508@gmail.com>
2017-02-17 16:21:33 -0800

    Update server.js

--- server/server.js
+++ server/server.js
@@ -12,5 +12,5 @@ app.use(express.static('./../public'));
 var port = 3000;
 
 app.listen(port, () => {
-	console.log('Now listening on port: ', port);
+  console.log('Now listening on port: ', port);
 });
