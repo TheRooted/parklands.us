6aa4a08a28e748eca6ee4f80595fadf11a92cc45
Kay Christensen <kaychristensen@gmail.com>
2017-02-20 13:48:15 -0800

    Add signup, signin, signout functions in controller

--- server/controller.js
+++ server/controller.js
@@ -3,5 +3,24 @@ var db = require('../db/schema');
 
 module.exports = {
 
+  signup: {
+    post: function() {
+      console.log('signup post');
+    }
+  },
+
+  signin: {
+    post: function() {
+      console.log('signin post')
+    }
+  },
+
+  signout: {
+    post: function() {
+      console.log('singout post')
+    }
+  },
+
+
 
 };
