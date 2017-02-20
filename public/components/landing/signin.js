b46249aac4a5cb033b5c9ad7c3ffc6ce45d3acbb
Brian <brianjschultz508@gmail.com>
2017-02-20 15:17:03 -0800

    replace indents with spaces

--- public/components/landing/signin.js
+++ public/components/landing/signin.js
@@ -3,34 +3,34 @@ import axios from 'axios';
 //need links from react-router?
 
 export default class Signin extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {};
-	}
-	handleSubmit() {
-		var user = {
-			email: this.refs.emailSI.value,
-			password: this.refs.passwordSI.value
-		}
-		axios.post('/api/signin', user).then(function(res) {
-			if (res.data) {
-				console.log('successful signin');
-			}
-		})
-	}
-	render() {
-		return (
-			<div>
-				<form>
-					<h5>Sign In!</h5>
-					<input type="email" name="email" placeholder="Email Address" ref="emailSI" />
-					<br />
-					<input type="password" name="password" placeholder="Password" ref="passwordSI" />
-					<br />
-					<button type="submit" onClick={this.handleSubmit.bind(this)}>Sign In!</button>
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  handleSubmit() {
+    var user = {
+      email: this.refs.emailSI.value,
+      password: this.refs.passwordSI.value
+    }
+    axios.post('/api/signin', user).then(function(res) {
+      if (res.data) {
+        console.log('successful signin');
+      }
+    })
+  }
+  render() {
+    return (
+      <div>
+        <form>
+          <h5>Sign In!</h5>
+          <input type="email" name="email" placeholder="Email Address" ref="emailSI" />
+          <br />
+          <input type="password" name="password" placeholder="Password" ref="passwordSI" />
+          <br />
+          <button type="submit" onClick={this.handleSubmit.bind(this)}>Sign In!</button>
 
-				</form>
-			</div>
-		)
-	}
+        </form>
+      </div>
+    )
+  }
 }
--- public/components/landing/signup.js
+++ public/components/landing/signup.js
@@ -3,40 +3,40 @@ import axios from 'axios';
 //need links from react-router?
 
 export default class Signup extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {};
-	}
-	handleSubmit() {
-		var user = {
-			firstName: this.refs.firstNameSI.value,
-			lastName: this.refs.lastNameSI.value,
-			email: this.refs.emailSI.value,
-			password: this.refs.passwordSI.value
-		}
-		axios.post('/api/signup', user).then(function(res) {
-			if (res.data) {
-				console.log('successful signin');
-			}
-		})
-	}
-	render() {
-		return (
-			<div>
-				<form>
-					<h5>Sign Up!</h5>
-					<input type="text" name="firstName" placeholder="First Name" ref="firstNameSI" />
-					<br />
-					<input type="text" name="lastName" placeholder="Last Name" ref="lastNameSI" />
-					<br />
-					<input type="email" name="email" placeholder="Email Address" ref="emailSI" />
-					<br />
-					<input type="password" name="password" placeholder="Password" ref="passwordSI" />
-					<br />
-					<button type="submit" onClick={this.handleSubmit.bind(this)}>Sign Up!</button>
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  handleSubmit() {
+    var user = {
+      firstName: this.refs.firstNameSI.value,
+      lastName: this.refs.lastNameSI.value,
+      email: this.refs.emailSI.value,
+      password: this.refs.passwordSI.value
+    }
+    axios.post('/api/signup', user).then(function(res) {
+      if (res.data) {
+        console.log('successful signin');
+      }
+    })
+  }
+  render() {
+    return (
+      <div>
+        <form>
+          <h5>Sign Up!</h5>
+          <input type="text" name="firstName" placeholder="First Name" ref="firstNameSI" />
+          <br />
+          <input type="text" name="lastName" placeholder="Last Name" ref="lastNameSI" />
+          <br />
+          <input type="email" name="email" placeholder="Email Address" ref="emailSI" />
+          <br />
+          <input type="password" name="password" placeholder="Password" ref="passwordSI" />
+          <br />
+          <button type="submit" onClick={this.handleSubmit.bind(this)}>Sign Up!</button>
 
-				</form>
-			</div>
-		)
-	}
+        </form>
+      </div>
+    )
+  }
 }
