2bd369a8ce2d3e95e13bd52bc3e45f884aa6c9ce
Brian <brianjschultz508@gmail.com>
2017-02-19 16:04:19 -0800

    test - server spec test template set up

---
+++ Spec/server/serverSpec.js
@@ -0,0 +1,29 @@
+var expect = require('chai').expect;
+var request = require('request');
+
+// describe('', function() {
+// 	//sections could include testing signin / logout / routing / etc
+// 	describe('Section of testing', function() {
+// 		it('Specific test', function(done) {
+// 			var options = {
+// 				'method': 'POST', //post / get
+// 				'uri': 'http://127.0.0.1:3000/somePath', //paths could be /api/signin , /api/logout , etc
+// 				'json': { // this is only needed if we are posting data
+// 					'datatype': 'send data in here' //datatype would need to be changed
+// 				}
+// 			};
+// 			request(options, function(error, res, body) { //simple request
+// 				expect(res.statusCode).to.not.be.null; 
+// 				done();
+// 			})
+// 		});
+// 	});
+// });
+
+
+
+
+
+
+
+
