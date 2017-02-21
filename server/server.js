a0dc8177eb1fb7d5f01c7433de3c0e93a96008a4
Kay Christensen <kaychristensen@gmail.com>
2017-02-20 19:53:52 -0800

    Configure passport

--- server/server.js
+++ server/server.js
@@ -1,19 +1,75 @@
 var express = require('express');
+var session = require('express-session');
 var parser = require('body-parser');
 var passport = require('passport');
 var router = require('./routes');
-
+var LocalStrategy = require('passport-local').Strategy;
+var db = require('../db/schema');
+var bcrypt = require('bcrypt');
+// var RedisStore = require('connect-redis')(session)
 
 var app = express();
 
-// middleware
+
+// Passport middleware
 app.use(parser.json());
 
-// routing
+app.use(session({  
+  secret: 'saucecat',
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.use(new LocalStrategy({
+    usernameField: 'email',
+    passwordField: 'password'
+  },
+  function(username, password, done) {
+    process.nextTick(function() {
+      db.models.user.findOne({ where: { email: username }})
+      .then(function(user, err) {
+        console.log('user is', user);
+        if (err) { return done(err); }
+        if (!user.dataValues.email) {
+          console.log('in email');
+          return done(null, false, { message: 'Incorrect email.' });
+        }
+        bcrypt.compare(password, user.dataValues.password, function(err, comparison) {
+          if (err) {
+            console.log('Error in password comparison', err);
+          }
+          // Supplied password matches, user already has account; send to Signin page
+          if (comparison === false) {
+            return done(null, false, { message: 'Incorrect password.' });
+          // Supplied pw doesn't match; probably new user & should choose another username
+          } else {
+            console.log('everything was okay!')
+            return done(null, user.dataValues);
+            // return done(user.dataValues);
+          }
+        });
+      });
+    })
+  }
+));
+
+// Routing
 app.use('', router);
 
-// serve static file
+// Serve static file
 app.use(express.static('public'));
+
 
 var port = 3000;
 
