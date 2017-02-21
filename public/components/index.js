b348f1721250dd948a64e6396f3bc6ad82762cae
Jackie <jackieh.bee@gmail.com>
2017-02-20 20:42:46 -0800

    fix routing

--- public/components/index.js
+++ public/components/index.js
@@ -1,8 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App.js'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.js';
+import { Router, Route, browserHistory, IndexRoute} from 'react-router';
+
+import Signin from './landing/signin.js';
+import Signup from './landing/signup.js';
 
 const app = document.getElementById('app');
 
+ReactDOM.render((
+  <Router history={browserHistory}>
+    <Route path='/' component={App} />
+    <Route path='signin' component={Signin} />
+    <Route path='signup' component={Signup} />
 
-ReactDOM.render(<App />, app)
+  </Router>), app);
