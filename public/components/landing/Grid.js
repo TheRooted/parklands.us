e048299a35f717bfd0c7d8ddfda818c643f02e75
Brian <brianjschultz508@gmail.com>
2017-02-20 20:03:37 -0800

    changed tabs to spaces

--- public/components/landing/Grid.js
+++ public/components/landing/Grid.js
@@ -3,28 +3,28 @@ import axios from 'axios';
 var GridElement = require('./GridElement');
 
 export default class Grid extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			parks: [{name: 'Yosemite', rating: 0}],
-		};
-	}
-	componentWillMount() {
-		var context = this;
-		axios.get('/api/grid').then(function(res) {
-			if (res.data) {
-				console.log('parks', res.data);
-				context.setState({parks: res.data})
-			}
-		});
-	}
-	render () {
-		return (
-			<div>
-				{this.state.parks.map(park =>
-					<GridElement parkName={park.name} parkRating={park.rating} />
-				)}
-			</div>
-		)
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      parks: [{name: 'Yosemite', rating: 0}],
+    };
+  }
+  componentWillMount() {
+    var context = this;
+    axios.get('/api/grid').then(function(res) {
+      if (res.data) {
+        console.log('parks', res.data);
+        context.setState({parks: res.data})
+      }
+    });
+  }
+  render () {
+    return (
+      <div>
+        {this.state.parks.map(park =>
+          <GridElement parkName={park.name} parkRating={park.rating} />
+        )}
+      </div>
+    )
+  }
 }
