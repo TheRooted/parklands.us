8ab5c3e36221f2c74836496fdb1c5ef8eecd9cea
Kay Christensen <kaychristensen@gmail.com>
2017-02-20 16:39:04 -0800

    :

--- server/server.js
+++ server/server.js
@@ -1,5 +1,8 @@
+var db = require('../db/schema');
 var express = require('express');
 var parser = require('body-parser');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 var router = require('./routes');
 
 
@@ -7,12 +10,29 @@ var app = express();
 
 // middleware
 app.use(parser.json());
-
-// routing
+app.use(express.static('public'));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use('', router);
 
-// serve static file
-app.use(express.static('public'));
+passport.use(new LocalStrategy({
+    usernameField: 'email',
+    passwordField: 'password'
+  },
+  function(username, password, done) {
+    db.models.user.findOne({ email: email }, function (err, user) {
+      if (err) { return done(err); }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      return done(null, user);
+    });
+  }
+));
+
 
 var port = 3000;
 
