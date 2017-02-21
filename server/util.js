3de769d3763095a6791eb7e004347a9ccbb2ddc8
Kay Christensen <kaychristensen@gmail.com>
2017-02-20 19:54:03 -0800

    Implement createSession function

--- server/util.js
+++ server/util.js
@@ -1,15 +1,15 @@
-var isLoggedIn = function(req) {
-  return req.session ? !!req.session.user : false;
-};
+// var isLoggedIn = function(req) {
+//   return req.session ? !!req.session.user : false;
+// };
 
-module.exports.checkUser = function(req, res, next){
-  if (!isLoggedIn(req)) {
-    // User must be redirected to Signin page
-    res.sendStatus(401);
-  } else {
-    next();
-  }
-};
+// module.exports.checkUser = function(req, res, next){
+//   if (!isLoggedIn(req)) {
+//     // User must be redirected to Signin page
+//     res.sendStatus(401);
+//   } else {
+//     next();
+//   }
+// };
 
 module.exports.createSession = function(req, res, newUser) {
   return req.session.regenerate(function() {
