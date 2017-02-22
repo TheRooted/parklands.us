7e28c020be6ca2e1e3c321c39233d83081d2b98d
Tenzin Sonam <tenso2006@gmail.com>
2017-02-21 18:44:54 -0800

    Add: sidebar component

---
+++ public/components/userTimeline/sidebar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Sidebar = (props) => {
+
+  return (
+    <div id="sideBarContainer">
+
+      <a href="#">Map</a><br />
+      <a href="#">Donate</a>
+    </div>
+  );
+};
+
+export default Sidebar;
