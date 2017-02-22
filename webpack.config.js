1c2e2f9c78f72524d5616e1064194d6d2a31e142
Kay Christensen <kaychristensen@gmail.com>
2017-02-22 15:22:40 -0800

    Replace webpack config to fix react mapbox-gl bug

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,36 @@
-var path = require('path');
-
+var webpack = require('webpack')
+var path = require('path')
+ 
 module.exports = {
   entry: './public/components/index.js',
-  output: {
-    path: __dirname + '/public',
-    filename: 'bundle.js'
+  output: { path: __dirname + '/public', filename: 'bundle.js' },
+  resolve: {
+    extensions: ['', '.js'],
+    alias: {
+      webworkify: 'webworkify-webpack',
+      'mapbox-gl': path.resolve('./node_modules/mapbox-gl/dist/mapbox-gl.js')
+    }
   },
   module: {
     loaders: [
       {
-        test: /\.jsx?/,
-        loaders: [
-          'babel-loader?presets[]=react,presets[]=es2015'
-        ],
+        test: /\.jsx?$/,
+        loaders: ['babel-loader?presets[]=react,presets[]=es2015'],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.json$/,
+        loader: 'json-loader'
+      },
+      {
+        test: /\.js$/,
+        include: path.resolve(__dirname, 'node_modules/webworkify/index.js'),
+        loader: 'worker'
+      },
+      {
+        test: /mapbox-gl.+\.js$/,
+        loader: 'transform/cacheable?brfs'
       }
     ]
-  }
-}
-//
+  },
+};
