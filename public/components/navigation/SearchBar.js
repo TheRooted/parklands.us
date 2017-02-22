2e8b656e311bd461a03022d76b3f67f172d90ce2
Jackie <jackieh.bee@gmail.com>
2017-02-21 19:53:22 -0800

    add burger bar

--- public/components/navigation/SearchBar.js
+++ public/components/navigation/SearchBar.js
@@ -1,13 +1,18 @@
 import React from 'react';
 
+
 const SearchBar = (props) => (
-	<div id="wrap">
-	  <form action="">
-	   <input id="search" name="search" type="text" placeholder="Find your park here..."></input>
-	   <input id="search_submit" value="Rechercher" type="submit"></input>
-	  </form>
-	</div>
+  <div id="wrap">
+    <form action="">
+      <datalist id='parks'>
+        <option value='Yosemite'></option>
+        <option value='Yellowstone'></option>
+        <option value='Denali'></option>
+      </datalist>
+      <input id="search" type="text" placeholder="Find your park here..." list='parks' onChange={props.getQuery}></input>
+      <input id="search_submit" value="Rechercher" type="submit" onClick={props.handleSubmit}></input>
+    </form>
+  </div>
 )
 
 export default SearchBar;
-
--- public/components/navigation/nav.js
+++ public/components/navigation/nav.js
@@ -3,13 +3,37 @@ import SearchBar from './SearchBar.js';
 import BurgerBar from './BurgerBar.js';
 
 
-const Nav = (props) => (
-  <div className='navigation'>
-    <SearchBar />
-    <span>Parklands</span>
-    <BurgerBar />
-    
-  </div>
-)
+class Nav extends React.Component {
+  constructor(props) {
+    super(props);
+      this.state = {
+        query: ''
+      }
+    }
+
+  getQuery(event) { 
+    this.setState({query: event.target.value})
+  } 
+
+  handleSubmit(event) {
+    // based on input, redirect to specific page.
+    console.log('beans');
+    event.preventDefault();
+
+  }
+
+  render () { 
+    return (
+      <div className='navigation'>
+        <SearchBar 
+          getQuery={this.getQuery.bind(this)}
+          handleSubmit={this.handleSubmit.bind(this)}
+        />
+        <span>Parklands</span>
+        <BurgerBar />
+     </div>
+    )
+  }
+}
 
 export default Nav;
