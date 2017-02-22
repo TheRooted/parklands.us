d8f35603806fe7d0e66bc084f1981a33068ef909
Brian <brianjschultz508@gmail.com>
2017-02-22 12:10:19 -0800

    fix tabs in parkcomment

--- public/components/snp/Parkcomment.js
+++ public/components/snp/Parkcomment.js
@@ -2,10 +2,10 @@ import React from 'react';
 import axios from 'axios';
 
 var Parkcomment = (props) => (
-    <div>
-      <p className='parkcommentusername'>{props.userEmail} says: </p>
-      <p className='parkcomment'>{props.text}</p>
-    </div>
+  <div>
+    <p className='parkcommentusername'>{props.userEmail} says: </p>
+    <p className='parkcomment'>{props.text}</p>
+  </div>
 );
 
 
