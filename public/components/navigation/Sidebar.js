f8220b00f26d8eb1b8ffcbcd3888d3dbdd9d349f
Jackie <jackieh.bee@gmail.com>
2017-02-23 11:00:48 -0800

    refactor component to es6, fix css bugs

--- public/components/navigation/Sidebar.js
+++ public/components/navigation/Sidebar.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router';
 
-const Sidebar = React.createClass({
-  render: function() {
-    let sidebarClass = this.props.isOpen ? 'sidebar open' : 'sidebar';
+class Sidebar extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  sidebarAddClass() {
+    var sidebarClass = this.props.isOpen ? 'sidebar open' : 'sidebar';
+    return sidebarClass;
+  }
+
+  render() {
     return (
       <div>
         <img src='https://www.newfangled.com/wp-content/uploads/2014/08/stuffcontentmgrfiles2ac67e44c30a21635f8a9d498f832bc1cmisc_resized80_313_257_hamenu.png' 
           onClick={this.props.toggleSidebar} className="burger-button" />
-        <div className={sidebarClass}>
+        <div className={this.sidebarAddClass()}>
           <div className="sidebarContent">Username_here</div>
           <Link to='userTimeline' className="sidebarContent">Timeline</Link>
           <div className="sidebarContent">Feed</div>
@@ -18,10 +26,7 @@ const Sidebar = React.createClass({
       </div>
     );
   }
-});
-
-// <img src='https://www.newfangled.com/wp-content/uploads/2014/08/stuffcontentmgrfiles2ac67e44c30a21635f8a9d498f832bc1cmisc_resized80_313_257_hamenu.png' className='burger' width='22' height='22'
-//   onClick={this.props.toggleSidebar}
+}
 
 export default Sidebar;
 
