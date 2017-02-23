952563f2c67df809cee4466f1a66556d8f225bf7
Kay Christensen <kaychristensen@gmail.com>
2017-02-23 10:56:14 -0800

    Remove map route

--- public/components/index.js
+++ public/components/index.js
@@ -9,7 +9,6 @@ import Signup from './authPages/signup.js';
 import UserTimeline from './userTimeline/user_timeline.js';
 import Landing from './landing/Landing.js';
 import Snp from './snp/Snp.js'
-import Map from './map.js';
 
 
 const app = document.getElementById('app');
@@ -22,7 +21,6 @@ ReactDOM.render((
       <Route path='signup' component={Signup} />
       <Route path='userTimeline' component={UserTimeline} />
       <Route path='park/:parkName' component={Snp} />
-      <Route path='map' component={Map} />
     </Route>
 
   </Router>), app);
