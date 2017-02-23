90f3d5eaa1c1122bb37913448f4e23d69a11e629
Brian <brianjschultz508@gmail.com>
2017-02-23 11:24:39 -0800

    park comment merge conflict resolved

--- public/components/snp/Parkcomment.js
+++ public/components/snp/Parkcomment.js
@@ -2,11 +2,7 @@ import React from 'react';
 import axios from 'axios';
 
 var Parkcomment = (props) => (
-<<<<<<< HEAD
   <div className= 'parkcommentContainer'>
-=======
-  <div className='parkcommentcontainer'>
->>>>>>> move signin signup to authPages dir
     <p className='parkcommentusername'>{props.userEmail} says: </p>
     <p className='parkcomment'>{props.text}</p>
   </div>
