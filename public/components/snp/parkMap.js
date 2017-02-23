5871baa01025c3824f48e7f3495131aa63ea7a14
Kay Christensen <kaychristensen@gmail.com>
2017-02-23 12:22:11 -0800

    Create park-specific map

---
+++ public/components/snp/parkMap.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import mapboxgl from 'mapbox-gl';
+
+export default class Mapp extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  componentDidMount() {
+    this.createMap();
+  }
+
+  createMap() {
+    var parkMap = window.document.getElementById('map');
+    var script = window.document.createElement('script');
+    script.type = 'text/javascript';
+    script.text = "mapboxgl.accessToken = 'pk.eyJ1Ijoic3Blc2NoZWxsayIsImEiOiJjaXo4bXB2cG8wMHA2MnZxbzNneHlicnZyIn0.K9hcDggIDFrtjjVS8LOXdA';varmap = new mapboxgl.Map({container: 'map',style: 'mapbox://styles/mapbox/satellite-streets-v10',minZoom: 3.5,maxZoom: 20,center: [-96, 39.5]});";
+    map.appendChild(script);
+  }
+
+  render() {
+    return (
+      <div className="parkMap" style={{width:'20vw', height:'10vh'}}></div>
+    )
+  }
+}
