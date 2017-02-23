c445523aaeca76508314af3a0a1c244a4a7e9583
Brian <brianjschultz508@gmail.com>
2017-02-23 11:37:00 -0800

    grid now capitalizes first letter in park name

--- public/components/landing/GridElement.js
+++ public/components/landing/GridElement.js
@@ -1,14 +1,22 @@
 import React from 'react';
 
-var GridElement = (props) => (
-  <div className='gridElement'>
-    <h5>{props.parkName}</h5>
-    {/* Need to add gridphoto to database
-    <img src={props.park.gridphoto} />*/}
-    <h5>{props.parkRating}</h5>
-    {/*Rating currently represented as a number, and unable to rate*/}
-  </div>
-)
+var capFirstLetter = function (string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+var GridElement = (props) => {
+  var name = capFirstLetter(props.parkName)
+  return (
+    <div className='gridElement'>
+      <h5>{name}</h5>
+      {/* Need to add gridphoto to database
+      <img src={props.park.gridphoto} />*/}
+      <h5>{props.parkRating}</h5>
+      {/*Rating currently represented as a number, and unable to rate*/}
+    </div>
+  )
+
+}
 
 
 module.exports = GridElement;
