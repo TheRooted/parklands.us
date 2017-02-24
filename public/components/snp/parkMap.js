988c076524250db9cf1aea0f2540708580686585
Kay Christensen <kaychristensen@gmail.com>
2017-02-24 14:05:00 -0800

    Replace script with JS and stringify

--- public/components/snp/parkMap.js
+++ public/components/snp/parkMap.js
@@ -10,19 +10,60 @@ export default class ParkMap extends React.Component {
     this.createMap();
   }
 
+  stringScript() {
+    var text = mapboxgl.accessToken = 'pk.eyJ1Ijoic3Blc2NoZWxsayIsImEiOiJjaXo4bXB2cG8wMHA2MnZxbzNneHlicnZyIn0.K9hcDggIDFrtjjVS8LOXdA';
+
+    var map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/mapbox/satellite-streets-v10',
+      center: [-119.5383, 37.8651],
+      minZoom: 6,
+      maxZoom: 20
+    });
+
+    map.on('load', function() {
+
+      // Add a layer showing the places.
+      map.addLayer({
+        "id": "parks",
+        "type": "symbol",
+        "source": {
+          "type": "geojson",
+          "data": {
+            "type": "FeatureCollection",
+            "features": [{
+              "type": "Feature",
+              "properties": {
+                "icon": "star"
+            },
+            "geometry": {
+              "type": "Point",
+              "coordinates": [-119.5383, 37.8651]
+            }
+            }]
+          }
+        },
+        "layout": {
+          "icon-image": "{icon}-15",
+          "icon-allow-overlap": true
+        }
+      });
+    });
+  }
+
   // function to find coordinates of park
 
   createMap() {
-    var parkMap = window.document.getElementById('parkMap');
+    var map = window.document.getElementById('map');
     var script = window.document.createElement('script');
     script.type = 'text/javascript';
-    script.text = "mapboxgl.accessToken = 'pk.eyJ1Ijoic3Blc2NoZWxsayIsImEiOiJjaXppdmZvaXcwMnYxMzNtazZkb2lnbDBqIn0.Dam1trKbpWhxWdh_5GQVrw'; var parkmap = new mapboxgl.Map({container: 'parkMap', style: 'mapbox://styles/mapbox/satellite-streets-v10', minZoom: 5, maxZoom: 20, center: [-119.5383, 37.8651]});";
-    parkMap.appendChild(script);
+    script.text = JSON.stringify(this.stringScript());
+    map.parentNode.insertBefore(script, map.nextSibling);
   }
 
   render() {
     return (
-      <div id="parkMap" style={{width:'300px', height:'300px', float:'left', marginRight:'14px', marginBottom:'5px'}}></div>
+      <div id="map" style={{width:'300px', height:'300px', float:'left', marginRight:'14px', marginBottom:'5px'}}></div>
     )
   }
 }
