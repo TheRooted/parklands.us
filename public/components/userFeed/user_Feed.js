907545dbd7a148d0c682992942f6ac947a32fb33
Tenzin Sonam <tenso2006@gmail.com>
2017-02-24 21:50:54 -0800

    feat: all user feed

--- public/components/userFeed/user_Feed.js
+++ public/components/userFeed/user_Feed.js
@@ -1,21 +1,51 @@
 import React from 'react';
 import axios from 'axios';
-import SocialMediaFeed from './socialMediaFeed.js'
+import SocialMediaFeed from './socialMediaFeed.js';
+import ParklandsUserFeed from './parklandsUsersFeed.js';
+import Post from './../userTimeline/post.js';
 
 export default class UserFeed extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
-
+      allFeed: []
     };
+  }
+
+  componentWillMount() {
+    const context = this;
+
+    axios.get('/api/userfeed')
+    .then(function (res) {
+      context.setState({
+        allFeed: res.data
+      });
+      console.log('res from userFeed is ', context.state.allFeed);
+    });
   }
 
   render () {
     return (
       <div id="user-feed-container">
         <h3>Popular Explorations</h3>
-        <SocialMediaFeed />
+        <div id="social-media-feed">
+          <SocialMediaFeed />
+        </div>
+        {/*<ParklandsUserFeed
+          allFeed={context.state.allFeed}
+        />*/}
+        <div id="all-feed">
+          {
+            this.state.allFeed.map(feed =>
+              <Post
+                key={feed.id}
+                photoData={feed.filePath}
+                datePosted={feed.createdAt}
+              />
+            )
+          }
+        </div>
         <div id="current-view"></div>
         <div id="media-feed-container"></div>
       </div>
