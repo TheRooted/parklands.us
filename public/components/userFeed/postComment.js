ddf279f5894fca77b871ed1eb14ef38d178067ba
Tenzin Sonam <tenso2006@gmail.com>
2017-02-26 16:32:51 -0800

    set: request for comment data

--- public/components/userFeed/postComment.js
+++ public/components/userFeed/postComment.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import axios from 'axios';
+import Comment from './comment.js';
 
 export default class CommentBox extends Component {
   constructor(props) {
@@ -11,28 +12,39 @@ export default class CommentBox extends Component {
     };
   }
 
+  componentWillMount() {
+    const context = this;
+    axios.get('/api/postcomment')
+    .then(function (res) {
+      context.setState({
+        comments: res.data
+      });
+      console.log('comment list is ', context.state.comments);
+    });
+  }
+
   render () {
-    //const comments = this._getComments();
+    const comments = this._getComments();
     return (
       <div className="comment-box">
         <div>
-
+          {comments}
         </div>
       </div>
     );
   }
 
-  // _getComments() {
-  //   return this.state.comments.map((comment) => {
-  //     return (
-  //       <Comment
-  //         id: {comment.id}
-  //         text: {comment.text}
-  //         userId: {comment.userId}
-  //         postId: {postId}
-  //         key: {comment.id}
-  //       />
-  //     )
-  //   })
-  // }
+  _getComments() {
+    return this.state.comments.map((comment) => {
+      return (
+        <Comment
+          id={comment.id}
+          text={comment.text}
+          userId={comment.userId}
+          postId={comment.postId}
+          key={comment.id}
+        />
+      )
+    })
+  }
 }
