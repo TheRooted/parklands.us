48b31dd64c56a73b7147f04af5879e27cc3ee268
Jackie <jackieh.bee@gmail.com>
2017-02-26 22:25:53 -0800

    add lightbox

--- public/components/snp/Parkphoto.js
+++ public/components/snp/Parkphoto.js
@@ -1,9 +1,56 @@
 import React from 'react';
+import Lightbox from 'react-image-lightbox';
 
-var ParkPhoto = (props) => (
-  <div className='parkPhotoContainer'>
-    <img className='official-parkphoto' src={props.photo} /> 
-  </div>
-);
+
+class ParkPhoto extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOpen: false,
+      photoIndex: this.props.index,
+      allPhotos: []
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.setState({allPhotos: nextProps.allPhotos})
+    this.setState({photoIndex: nextProps.index})
+  }
+
+  openLightbox () {
+    this.setState({isOpen: true})
+  }
+
+  closeLightbox () {
+    this.setState({isOpen: false})
+  }
+
+
+  render () {
+
+    return (
+      <div className='offical-park-modal' >
+        <img className='official-parkphoto' src={this.props.photo} onClick={this.openLightbox.bind(this)} />
+
+        {this.state.isOpen &&
+          <Lightbox
+            mainSrc={this.state.allPhotos[this.state.photoIndex]}
+            imageCaption={this.props.parkName + ' National Park'}
+            onCloseRequest={this.closeLightbox.bind(this)}
+            nextSrc={this.state.allPhotos[(this.state.photoIndex + 1) % this.state.allPhotos.length]}
+            prevSrc={this.state.allPhotos[(this.state.photoIndex + this.state.allPhotos.length - 1) % this.state.allPhotos.length]}
+            onMovePrevRequest={() => this.setState({
+                photoIndex: (this.state.photoIndex + this.state.allPhotos.length - 1) % this.state.allPhotos.length
+            })}
+            onMoveNextRequest={() => this.setState({
+                photoIndex: (this.state.photoIndex + 1) % this.state.allPhotos.length
+            })}
+          />
+          }
+      </div>
+    );
+  }
+
+}
 
 export default ParkPhoto;
