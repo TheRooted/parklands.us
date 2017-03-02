ee4c383a50313327bd3472f494af65bf0fcd06e9
Brian <brianjschultz508@gmail.com>
2017-03-01 20:06:15 -0800

    set dirname in routes

--- server/routes.js
+++ server/routes.js
@@ -1,49 +1,50 @@
 var controller = require('./controller');
 var passport = require('./passport');
 var router = require('express').Router();
+var path = require('path');
 
 
 // Routes for signup, signin, and signout
-router.post('/signup', passport.authenticate('local-signup', { successRedirect: '/', failureRedirect: '/signup' }));
-router.post('/signin', passport.authenticate('local-signin', { successRedirect: '/', failureRedirect: '/signin' }));
-router.post('/signout', passport.authenticate('local-signout', { successRedirect: '/signin', failureRedirect: '/signin' }));
+router.post(__dirname + '/signup', passport.authenticate('local-signup', { successRedirect: '/', failureRedirect: '/signup' }));
+router.post(__dirname + '/signin', passport.authenticate('local-signin', { successRedirect: '/', failureRedirect: '/signin' }));
+router.post(__dirname + '/signout', passport.authenticate('local-signout', { successRedirect: '/signin', failureRedirect: '/signin' })); 
 
 // Route to check and get active session user
-router.get('/api/session', controller.session.get);
+router.get(__dirname + '/api/session', controller.session.get);
 
 // Routes for parks
-router.get('/api/grid', controller.grid.get);
-router.get('/api/park/:parkName', controller.park.get);
-router.get('/api/parkPhoto/:id', controller.parkPhoto.get);
-router.get('/api/parkPhotoPost/:id', controller.parkPhotoPost.get);
-router.get('/api/parkComment/:id', controller.parkComment.get);
-router.get('/api/parkAverageRating/:id', controller.parkAverageRating.get);
-//Routes for snp Page
-router.get('/api/rating', controller.rating.get);
+router.get(__dirname + '/api/grid', controller.grid.get);
+router.get(__dirname + '/api/park/:parkName', controller.park.get);
+router.get(__dirname + '/api/parkPhoto/:id', controller.parkPhoto.get);
+router.get(__dirname + '/api/parkPhotoPost/:id', controller.parkPhotoPost.get);
+router.get(__dirname + '/api/parkComment/:id', controller.parkComment.get);
 
-router.post('/api/rating', controller.rating.post);
+//Routes for snp Page
+router.get(__dirname + '/api/rating', controller.rating.get);
+
+router.post(__dirname + '/api/rating', controller.rating.post);
 
 // router.get('/api/parkAlert/:id', controller.parkAlert.get);
 
 // Routes for user timeline
-router.get('/api/userTimeline', controller.userTimeline.get);
-router.post('/api/userTimeline', controller.userTimeline.post);
+router.get(__dirname + '/api/userTimeline', controller.userTimeline.get);
+router.post(__dirname + '/api/userTimeline', controller.userTimeline.post);
 
 // Routes for likes on photos
-router.get('/api/photoLike', controller.photoLike.get);
-router.post('/api/photoLike', controller.photoLike.post);
+router.get(__dirname + '/api/photoLike', controller.photoLike.get)
+router.post(__dirname + '/api/photoLike', controller.photoLike.post);
 
 // Routes for user user feed
-router.get('/api/userfeed', controller.userfeed.get);
+router.get(__dirname + '/api/userfeed', controller.userfeed.get);
 
 // Routes for post comment
-router.get('/api/postcomment', controller.postcomment.get);
+router.get(__dirname + '/api/postcomment', controller.postcomment.get);
 
 //Routes for user name
-router.get('/api/username', controller.username.get);
+router.get(__dirname + '/api/username', controller.username.get);
 
 // Routes for map
-router.get('/api/parklocations', controller.parklocations.get);
+router.get(__dirname + '/api/parklocations', controller.parklocations.get);
 
 
 module.exports = router;
