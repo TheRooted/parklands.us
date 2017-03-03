6f9e010dad5ec9dc597730d38f8474d46d1c5c29
Kay Christensen <kaychristensen@gmail.com>
2017-03-02 17:05:21 -0800

    Form changes

--- public/components/authPages/signup.js
+++ public/components/authPages/signup.js
@@ -8,37 +8,42 @@ export default class Signup extends React.Component {
     this.state = {};
   }
   
-  handleSubmit() {
-    var user = {
-      // firstName: this.refs.firstNameSI.value,
-      // lastName: this.refs.lastNameSI.value,
-      email: this.refs.emailSU.value,
-      password: this.refs.passwordSU.value
-    };
-    axios.post('/signup', user).then(function(res) {
-      if (res.request.responseURL === 'http://localhost:3000/') {
-        browserHistory.push('/');
-      } else (
-        browserHistory.push('/signup'));
-    });
+  validateForm() {
+
   }
+
+  // handleSubmit() {
+  //   var user = {
+  //     firstName: this.refs.firstNameSU.value,
+  //     lastName: this.refs.lastNameSU.value,
+  //     email: this.refs.emailSU.value,
+  //     password: this.refs.passwordSU.value
+  //   };
+  //   axios.post('/signup', user).then(function(res) {
+  //     console.log('handling submit')
+  //     if (res.request.responseURL === 'http://localhost:3000/') {
+  //       browserHistory.push('/');
+  //     } else (
+  //       browserHistory.push('/signup'));
+  //   });
+  // }
 
   render() {
     return (
       <div className='signupBg'>
-        <div className="auth-container signup">
-          <input className="auth-fields" type="text" name="firstName" placeholder="First" ref="firstSU" />
+        <form method="post" className="auth-container signup">
+          <input className="auth-fields" type="text" name="firstName" placeholder="First" ref="firstNameSU" />
           <br />
-          <input className="auth-fields" type="text" name="lastName" placeholder="Password" ref="lastSU" />
+          <input className="auth-fields" type="text" name="lastName" placeholder="Password" ref="lastNameSU" />
           <input className="auth-fields" type="email" name="email" placeholder="Email Address" ref="emailSU" />
           <br />
           <input className="auth-fields" type="password" name="password" placeholder="Password" ref="passwordSU" />
           <br />
           <div className="buttons">
-            <button className="btn-auth" onClick={this.handleSubmit.bind(this)}>Sign Up</button>
+            <button className="btn-auth" onClick={this.validateForm}>Sign Up</button>
             <Link to="/signin" className="signin-link">Already have an account? Sign in</Link>
           </div>
-        </div>
+        </form>
       </div>
     )
   }
