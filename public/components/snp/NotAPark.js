d763e112a7c794fb6e2fcc9ee5a227c9e4e53a2f
Kay Christensen <kaychristensen@gmail.com>
2017-03-03 16:13:19 -0800

    Style 'not a valid park' page

--- public/components/snp/NotAPark.js
+++ public/components/snp/NotAPark.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 var NotAPark = (props) => (
   <div id='notapark'>
-    <h1>{props.params.parkName} is not a valid park name!</h1>
+    <span className="invalid-message">Oops! We couldn't find a park called {props.params.parkName}.</span>
     <img src='https://s-media-cache-ak0.pinimg.com/originals/4b/77/17/4b7717ac59e7ed6582f5ae3251b1493e.jpg' />
   </div>
 )
