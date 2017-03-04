f1b426f0e657e76a78e2ed563fc5b7266cb53e25
Tenzin Sonam <tenso2006@gmail.com>
2017-03-04 12:46:15 -0800

    Refactored: PostComments to stateless functional component

--- public/components/userFeed/PostComments.js
+++ public/components/userFeed/PostComments.js
@@ -2,38 +2,27 @@ import React, {Component} from 'react';
 import axios from 'axios';
 import Comment from './Comment.js';
 
-export default class PostComment extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      showComments: false
-    };
-  }
-
-  render () {
-    const comments = this._getComments();
-    return (
-      <div className="comment-box">
-        <div>
-          {comments}
-        </div>
-      </div>
-    );
-  }
-
-  _getComments() {
-    return this.props.allComments.map((comment) => {
-      return (
-        <Comment
-          id={comment.id}
-          text={comment.text}
-          userId={comment.userId}
-          postId={comment.postId}
-          key={comment.id}
-          dateTime={comment.createdAt}
-        />
-      )
-    })
-  }
+const PostComment = (props) => {
+    // const comments = this._getComments();
+  return (
+    <div className="comment-box">
+        {
+          props.allComments.map((comment) => {
+            return (
+              <Comment
+                id={comment.id}
+                text={comment.text}
+                userId={comment.userId}
+                postId={comment.postId}
+                key={comment.id}
+                dateTime={comment.createdAt}
+              />
+            )
+          })
+        }
+    </div>
+  );
 }
+
+
+module.exports = PostComment;
