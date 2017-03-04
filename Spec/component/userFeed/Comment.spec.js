ad90238b29d601b9a08aa6afaee75ffcad98e13f
Tenzin Sonam <tenso2006@gmail.com>
2017-03-03 21:45:48 -0800

    Add: Post Comment test passes

--- Spec/component/userFeed/Comment.spec.js
+++ Spec/component/userFeed/Comment.spec.js
@@ -10,14 +10,8 @@ describe('<Comment>', () => {
     expect(wrapper.state('user')).to.equal('');
   });
 
-//   it('should have a button', function () {
-//     const wrapper = shallow(<Email/>);
-//     expect(wrapper.find('button')).to.have.length(1);
-//   });
-
-//   it('should have props for handleEmailChange and fetchGravatar', function () {
-//     const wrapper = shallow(<Email/>);
-//     expect(wrapper.props().handleEmailChange).to.be.defined;
-//     expect(wrapper.props().fetchGravatar).to.be.defined;
-//   });
+  it('should have a strong tag', function () {
+    const wrapper = shallow(<Comment/>);
+    expect(wrapper.find('strong')).to.have.length(1);
+  });
 });
