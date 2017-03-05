8cd1a675373fb107b47333e01b250f7e0b731ab0
Kay Christensen <kaychristensen@gmail.com>
2017-03-04 17:51:59 -0800

    Cursor point and hover grow on splash title

--- public/components/landing/Landing.js
+++ public/components/landing/Landing.js
@@ -15,8 +15,8 @@ export default class Landing extends React.Component {
     return (
       <div className="landing-div">
         <div className='splash-landing'>
-          <img className='splash-img' onClick={this.linkToMapView.bind(this)} src='https://www.adventure-journal.com/wp-content/uploads/2015/07/15155461297_402eeb3fd7_h.jpg' />
-          <div className='splash-title' onClick={this.linkToMapView.bind(this)}>START EXPLORING</div>
+          <img className='splash-img' src='https://www.adventure-journal.com/wp-content/uploads/2015/07/15155461297_402eeb3fd7_h.jpg' />
+          <div className='splash-title hvr-grow' onClick={this.linkToMapView.bind(this)}>START EXPLORING</div>
         </div>
         
       </div>
