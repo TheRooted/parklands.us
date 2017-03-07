bae74b638747f2642b46fd0be2f6b2eb2275b8df
Jackie <jackieh.bee@gmail.com>
2017-03-06 22:20:10 -0800

    credit tom for photo

--- public/components/landing/Landing.js
+++ public/components/landing/Landing.js
@@ -17,8 +17,9 @@ export default class Landing extends React.Component {
         <div className='splash-landing'>
           <img className='splash-img' src='https://www.adventure-journal.com/wp-content/uploads/2015/07/15155461297_402eeb3fd7_h.jpg' />
           <div className='splash-title hvr-grow' onClick={this.linkToMapView.bind(this)}>START EXPLORING</div>
+          <a className='tom-credit' href='http://www.travelcaffeine.com/'>Photo by Tom Bricker</a>
         </div>
-        
+
       </div>
     )
   }
