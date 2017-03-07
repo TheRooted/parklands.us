9b869c25ed88d57565333b2c2b04e05b6631cdd3
Kay Christensen <kaychristensen@gmail.com>
2017-03-06 20:18:14 -0800

    Make sidebar close on click or key press

--- public/components/navigation/BurgerBar.js
+++ public/components/navigation/BurgerBar.js
@@ -12,6 +12,34 @@ class BurgerBar extends React.Component {
     this.setState({sidebarOpen: !this.state.sidebarOpen})
   }
 
+  componentWillUpdate(nextProps, nextState) {
+    var context = this;
+    console.log('context', context)
+    // when the menu becomes visible, setup some handlers so we can close the menu easily
+    if (nextState.sidebarOpen == true) {
+      document.addEventListener('keydown', this.handleKeyPress.bind(context));
+      document.body.addEventListener('click', this.handleClick.bind(context));
+    } else {
+      document.removeEventListener('keydown', this.handleKeyPress.bind(context));
+      document.body.removeEventListener('click', this.handleClick.bind(context));
+    }
+  }
+
+  handleClick() {
+    console.log('handleclick')
+    if (this.state.sidebarOpen) {
+      // this.setState({ sidebarOpen: false });
+      this.toggleSidebar();
+    }
+  }
+
+  handleKeyPress(e) {
+    console.log('handleKeyPress', e);
+    if(e.keyCode === 13 && this.state.sidebarOpen) {
+      // this.setState({ sidebarOpen: false })
+      this.toggleSidebar();
+    }
+  }
 
   render() {
     return (
