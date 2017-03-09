f0e0ee2996b2f71cf57cb639fa8c48e5b62984d7
Brian <brianjschultz508@gmail.com>
2017-03-08 16:05:17 -0800

    sidebar now says signin when loggedout, signout when logged in

--- public/components/navigation/Sidebar.js
+++ public/components/navigation/Sidebar.js
@@ -5,7 +5,39 @@ import { browserHistory, Link } from 'react-router';
 class Sidebar extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {}
+    this.state = {
+      user: {},
+      signedIn: false,
+    }
+  }
+
+  componentWillMount () {
+    var context = this;
+    axios.get('/api/session').then(function(res) {
+      if (typeof res.data === 'object') {
+        context.setState({
+          user: res.data,
+          signedIn: true
+        })
+      }
+    })
+  }
+
+  componentWillReceiveProps (nextProps) {
+    var context = this;
+    axios.get('/api/session').then(function(res) {
+      if (typeof res.data === 'object') {
+        context.setState({
+          user: res.data,
+          signedIn: true
+        })
+      } else {
+        context.setState({
+          user: {},
+          signedIn: false
+        })
+      }
+    })
   }
 
   sidebarAddClass() {
@@ -14,18 +46,37 @@ class Sidebar extends React.Component {
   }
 
   handleSignout() {
-    axios.get('/api/session').then(function(res) {
-      var user = res.data;
+    var context = this;
+    axios.post('/signout', context.state.user).then(function(res) {
+      if (res.request.responseURL === 'http://localhost:3000/signin' || res.request.responseURL === 'http://127.0.0.1:3000/signin') {
+        browserHistory.push('/signin');
+      }
+    });
+  }
 
-      axios.post('/signout', user).then(function(res) {
-        if (res.request.responseURL === 'http://localhost:3000/signin') {
-          browserHistory.push('/signin');
-        }
-      });
-    })
+  handleSignin() {
+    browserHistory.push('/signin');
   }
 
   render() {
+    var context = this;
+    if (this.state.signedIn) {
+      var signOutOrSignIn = function () {
+        return (
+          <div className="sidebar-content">
+            <Link className="signoutlink" onClick={() =>{context.props.toggleSidebar(); context.handleSignout()}}>Sign Out</Link>
+          </div>
+        )
+      }
+    } else {
+      var signOutOrSignIn = function () {
+        return (
+          <div className="sidebar-content">
+            <Link className="signinlink" onClick={() =>{context.props.toggleSidebar(); context.handleSignin()}}>Sign In</Link>
+          </div>
+        )
+      }
+    }
     return (
       <div className="sidebarContainer">
         <img src='http://www.freeiconspng.com/uploads/menu-icon-13.png' 
@@ -41,9 +92,7 @@ class Sidebar extends React.Component {
             <div className="sidebar-content">
               <Link to="/profile" className="profilelink" onClick={this.props.toggleSidebar}>Profile</Link>
             </div>
-            <div className="sidebar-content">
-              <Link className="signoutlink" onClick={() =>{this.props.toggleSidebar(); this.handleSignout()}}>Sign Out</Link>
-            </div>
+            {signOutOrSignIn()}
           </div>
         </div>
       </div>
