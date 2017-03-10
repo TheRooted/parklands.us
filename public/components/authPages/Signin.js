58e143a4d2f912568637371ae3e2a2993cc19013
Kay Christensen <kaychristensen@gmail.com>
2017-03-09 20:50:03 -0800

    Update Signin.js

--- public/components/authPages/Signin.js
+++ public/components/authPages/Signin.js
@@ -52,7 +52,7 @@ export default class Signin extends React.Component {
   signinRequest(user) {
     axios.post('/signin', user).then((res) => {
       //LIVE FIXME: make sure 'http://parklands.us/signin' is an option below
-      if (res.request.responseURL === 'http://localhost:3000/signin' || res.request.responseURL === 'http://127.0.0.1:3000/signin' || res.request.responseURL === 'http://parklands.us/signin') {
+      if (res.request.responseURL === 'http://localhost:3000/signin' || res.request.responseURL === 'http://127.0.0.1:3000/signin' || res.request.responseURL === 'http://parklands.us/signin' || res.request.responseURL === 'http://www.parklands.us/signin') {
         this.showAlert();
       } else {
         browserHistory.push('/explore');
