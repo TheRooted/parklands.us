18d41b6b7b42ddf5a8c2b0bc26421b410be7c895
Jackie <jackieh.bee@gmail.com>
2017-03-16 20:54:34 -0700

    add buttons to nav bar

--- public/components/navigation/Sidebar.js
+++ public/components/navigation/Sidebar.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import axios from 'axios';
 import { browserHistory, Link } from 'react-router';
+import { Button, Icon } from 'semantic-ui-react';
+
 
 class Sidebar extends React.Component {
   constructor(props) {
@@ -67,7 +69,12 @@ class Sidebar extends React.Component {
       var signOutOrSignIn = function () {
         return (
           <div className="sidebar-content">
-            <Link className="signoutlink" onClick={() =>{context.props.toggleSidebar(); context.handleSignout()}}>Sign Out</Link>
+            <Link className="signoutlink" onClick={() =>{context.props.toggleSidebar(); context.handleSignout()}}>
+              <Icon
+                name={'sign out'}
+                inverted color='grey'
+              />
+              <span> Sign Out</span></Link>
           </div>
         )
       }
@@ -75,25 +82,48 @@ class Sidebar extends React.Component {
       var signOutOrSignIn = function () {
         return (
           <div className="sidebar-content">
-            <Link className="signinlink" onClick={() =>{context.props.toggleSidebar(); context.handleSignin()}}>Sign In</Link>
+            <Link className="signinlink" onClick={() =>{context.props.toggleSidebar(); context.handleSignin()}}>
+              <Icon
+                name={'sign in'}
+                inverted color='grey'
+              />
+              <span> Sign In</span>
+            </Link>
           </div>
         )
       }
     }
     return (
       <div className="sidebarContainer">
-        <img src='http://www.freeiconspng.com/uploads/menu-icon-13.png' 
+        <img src='http://www.freeiconspng.com/uploads/menu-icon-13.png'
           className="burger-button" onClick={this.props.toggleSidebar} />
         <div className={this.sidebarAddClass()}>
           <div className="sidebar-links">
             <div className="sidebar-content">
-              <Link to="/explore" className="maplink" onClick={this.props.toggleSidebar}>Explore</Link>
+              <Link to="/explore" className="maplink" onClick={this.props.toggleSidebar}>
+                <Icon
+                  name={'map signs'}
+                  inverted color='grey'
+                />
+                <span> Explore</span>
+              </Link>
             </div>
             <div className="sidebar-content">
-              <Link to="/trending" className="userfeedlink" onClick={this.props.toggleSidebar}>Trending</Link>
+              <Link to="/trending" className="userfeedlink" onClick={this.props.toggleSidebar}>
+                <Icon
+                  name={'leaf'}
+                  inverted color='grey'
+                />
+                <span> Trending</span>
+                </Link>
             </div>
             <div className="sidebar-content">
-              <Link to="/profile" className="profilelink" onClick={this.props.toggleSidebar}>Profile</Link>
+              <Link to="/profile" className="profilelink" onClick={this.props.toggleSidebar}>
+                <Icon
+                  name={'user'}
+                  inverted color='grey'
+                />
+                <span> Profile</span></Link>
             </div>
             {signOutOrSignIn()}
           </div>
--- public/components/snp/Info.js
+++ public/components/snp/Info.js
@@ -11,6 +11,7 @@ const Info = (props) => {
           <div>{line}</div>
       )}
 
+
     </div>
   )
 }
