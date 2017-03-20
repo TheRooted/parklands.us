b7cb13eef154fc19444c0d3e398128bb6d678e3b
Kay Christensen <kaychristensen@gmail.com>
2017-03-19 18:17:34 -0700

    Add responsive styling to landing page (#27)
    * Change goats to background-image
    
    * Center background img
    
    * Responsive styles on landing page

--- public/components/landing/Landing.js
+++ public/components/landing/Landing.js
@@ -23,12 +23,11 @@ export default class Landing extends React.Component {
     return (
       <div className="landing-div">
         <div className='splash-landing'>
-          <img className='splash-img' src='https://www.adventure-journal.com/wp-content/uploads/2015/07/15155461297_402eeb3fd7_h.jpg' />
           <div className='splash-title hvr-grow' onClick={this.linkToExplore.bind(this)}>START EXPLORING</div>
           <button className='splash-signin hvr-grow' onClick={this.signIn.bind(this)}>Sign In</button>
           <button className='splash-signup hvr-grow' onClick={this.signUp.bind(this)}>Sign Up</button>
-          <a className='tom-credit' href='http://www.travelcaffeine.com/' target='_blank'>Photo by Tom Bricker</a>
         </div>
+        <a className='tom-credit' href='http://www.travelcaffeine.com/' target='_blank'>Photo by Tom Bricker</a>
       </div>
     )
   }
